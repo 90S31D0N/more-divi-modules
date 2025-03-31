14738a5376000448eee41884534366b464e4f690
90S31D0N <noelschaller32@outlook.com>
2025-03-31 15:37:37 +0200

    create divi extension

---
+++ includes/MoreDiviModules.php
@@ -0,0 +1,46 @@
+<?php
+
+class MDM_MoreDiviModules extends DiviExtension {
+
+	/**
+	 * The gettext domain for the extension's translations.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $gettext_domain = 'mdm-more-divi-modules';
+
+	/**
+	 * The extension's WP Plugin name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $name = 'more-divi-modules';
+
+	/**
+	 * The extension's version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $version = '1.0.0';
+
+	/**
+	 * MDM_MoreDiviModules constructor.
+	 *
+	 * @param string $name
+	 * @param array  $args
+	 */
+	public function __construct( $name = 'more-divi-modules', $args = array() ) {
+		$this->plugin_dir     = plugin_dir_path( __FILE__ );
+		$this->plugin_dir_url = plugin_dir_url( $this->plugin_dir );
+
+		parent::__construct( $name, $args );
+	}
+}
+
+new MDM_MoreDiviModules;
---
+++ more-divi-modules.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Plugin Name: More Divi Modules
+Plugin URI:  
+Description: 
+Version:     1.0.0
+Author:      Noël Schaller
+Author URI:  
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: mdm-more-divi-modules
+Domain Path: /languages
+
+More Divi Modules is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+More Divi Modules is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with More Divi Modules. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+*/
+
+
+if ( ! function_exists( 'mdm_initialize_extension' ) ):
+/**
+ * Creates the extension's main class instance.
+ *
+ * @since 1.0.0
+ */
+function mdm_initialize_extension() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/MoreDiviModules.php';
+}
+add_action( 'divi_extensions_init', 'mdm_initialize_extension' );
+endif;
